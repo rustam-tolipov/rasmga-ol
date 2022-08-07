2753ae8a99850f00fb785c2f1921434a80918c28
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-07 22:58:21 +0500

    Fetch posts from real database

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -5,27 +5,27 @@ import './Posts.scss';
 import PostsSearch from './PostsSearch';
 
 const Posts = (props) => {
-  // fetch https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
-
   const [posts, setPosts] = useState([]);
 
-  console.log(process.env);
-
+  //fetch https://rustam-social-media-app.herokuapp.com/api/v1/posts
   useEffect(() => {
-    fetch(
-      `https://api.unsplash.com/search/photos?page=1&length=20&query=cats&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS}`
-    )
+    fetch('https://rustam-social-media-app.herokuapp.com/api/v1/posts')
       .then((response) => response.json())
-      .then((data) => setPosts(data.results));
+      .then((data) => {
+        setPosts(data);
+      });
   }, []);
+
+  console.log(posts);
 
   return (
     <div className='posts'>
       <PostsSearch />
       <h1>Posts</h1>
-      {posts.map((post, i) => {
-        return <Post key={i} url={post.urls.regular} />;
-      })}
+      {posts &&
+        posts.map((post, i) => {
+          return <Post key={posts.id} url={post.image.url} />;
+        })}
     </div>
   );
 };
