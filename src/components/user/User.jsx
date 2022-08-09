19403d1a5d962330d9c9fc40d95402b03be3eaa2
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-09 21:50:43 +0500

    Show information based on data

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -4,13 +4,30 @@ import UserProfile from './UserProfile';
 
 import './User.scss';
 
+const fakeData = {
+  email: 'maqsudtolipov9@gmail.com',
+  first_name: 'Maqsud',
+  last_name: 'Tolipov',
+  username: 'maqsudtolipov9',
+  description:
+    'Hi there. This is the first person in the list of people who have joined the group.',
+  avatar: {
+    url: 'https://i.ibb.co/rQS844Y/Blue-Minimalist-Circle-Profile-Image-1.png',
+  },
+};
+
 const User = () => {
   const { id } = useParams();
 
   return (
     <div className='user'>
       Halo amigo {id}
-      <UserProfile username={id} />
+      <UserProfile
+        username={fakeData.username}
+        email={fakeData.email}
+        img={fakeData.avatar.url}
+        description={fakeData.description}
+      />
     </div>
   );
 };
--- src/components/user/UserProfile.jsx
+++ src/components/user/UserProfile.jsx
@@ -1,7 +1,15 @@
+import './UserProfile.scss'
+
 const UserProfile = (props) => {
   return (
-    <div>
-      <h1>{props.username}</h1>
+    <div className='user-profile'>
+      <div className='user-profile__info'>
+        <img className='user-profile__img' src={props.img} alt='' />
+        <div>
+          <p className='user-profile__name'>{props.username}</p>
+          <p>{props.description}</p>
+        </div>
+      </div>
     </div>
   );
 };
