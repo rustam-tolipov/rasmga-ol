321164b59df76a285ae5846c52b26559aba5c719
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-10 07:01:57 +0500

    Featch post images from test api

--- src/components/user/UserPosts.jsx
+++ src/components/user/UserPosts.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from 'react';
+
 const UserPosts = () => {
-  return <div className='user-posts'>Yeah its me</div>;
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(
+        'https://api.unsplash.com/search/photos/?client_id=v9W4IHuxN78WsxO2WTNGUydKt9rmpWBiTWElN5OZ3aY&page=1&query=cats&per_page=20'
+      );
+      const posts = await response.json();
+      setPosts(posts.results);
+    };
+    fetchPosts();
+  }, []);
+
+  console.log(posts);
+
+  return (
+    <div className='user-posts'>
+      {posts.map((post, index) => (
+        <div>
+          <img src={post.urls.regular} alt='' />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default UserPosts;
