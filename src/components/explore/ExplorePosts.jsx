ac7a12b6fdd352da60da23b1c09404530d721975
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-11 11:50:05 +0500

    Style ExplorePosts component

--- src/components/explore/ExplorePosts.jsx
+++ src/components/explore/ExplorePosts.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 
+import './ExplorePosts.scss';
+
 const ExplorePosts = () => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     const fetchPosts = async () => {
       const response = await fetch(
-        'https://api.unsplash.com/search/photos/?client_id=v9W4IHuxN78WsxO2WTNGUydKt9rmpWBiTWElN5OZ3aY&page=1&query=car&per_page=40'
+        'https://api.unsplash.com/search/photos/?client_id=v9W4IHuxN78WsxO2WTNGUydKt9rmpWBiTWElN5OZ3aY&page=1&query=car&per_page=100'
       );
       const posts = await response.json();
       setPosts(posts.results);
@@ -16,7 +18,7 @@ const ExplorePosts = () => {
 
   console.log(posts);
   return (
-    <div>
+    <div className='explore-posts'>
       {posts.map((post, index) => {
         return <img src={post.urls.regular} alt='' />;
       })}
