76daa707610c5a18444ccb334338a957147674bf
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-24 21:51:31 +0500

    Complete upload feature

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,9 +1,42 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { createPortal } from 'react-dom';
+import Axios from 'axios';
+import Cookies from 'js-cookie';
+
+import Button from '../UI/Button';
+
 import './CreatePost.scss';
+import image from '../../assets/upload.svg';
 
 const CreatePost = (props) => {
+  const textareaRef = useRef(null);
+
   const [open, setOpen] = useState(false);
+  const [file, setFile] = useState(null);
+
+
+  const uploadHandler = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const postHandler = () => {
+    let formData = new FormData();
+
+    formData.append('image', file);
+    formData.append('content', textareaRef.current.value);
+
+    Axios.post(
+      'https://rustam-social-media-rails-app.herokuapp.com/api/v1/posts',
+      formData,
+      {
+        headers: {
+          Authorization: `Bearer ${Cookies.get('jwt')}`,
+        },
+      }
+    ).then((res) => {
+      console.log(res);
+    });
+  };
 
   // class if from PostOverlay component
   const backdrop = (
@@ -12,11 +45,24 @@ const CreatePost = (props) => {
       onClick={() => {
         setOpen(false);
       }}
-    >
-    </div>
+    ></div>
   );
 
-  const postOverlay = <div className='create-overlay'>aa</div>;
+  const postOverlay = (
+    <div className='create-overlay'>
+      <img className='create-overlay__icon' src={image} alt='' width='10px' />
+      <p className='create-overlay__text'>Drag photos here</p>
+      <input type='file' name='file' onChange={uploadHandler} />
+      <textarea
+        ref={textareaRef}
+        name='content'
+        placeholder='Whats on your mind?'
+        minLength='15'
+        maxLength='500'
+      ></textarea>
+      <Button onClick={postHandler}>Post your image</Button>
+    </div>
+  );
 
   return (
     <>
@@ -29,7 +75,8 @@ const CreatePost = (props) => {
         Post
       </button>
       {open && createPortal(backdrop, document.querySelector('#post-portal'))}
-      {open && createPortal(postOverlay, document.querySelector('#post-portal'))}
+      {open &&
+        createPortal(postOverlay, document.querySelector('#post-portal'))}
     </>
   );
 };
