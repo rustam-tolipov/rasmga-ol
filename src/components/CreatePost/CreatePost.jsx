56e0bffe9c4dc0793b0df70e9d5bcf09fb183ead
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-24 20:41:16 +0500

    create upload overlay

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import { createPortal } from 'react-dom';
 import './CreatePost.scss';
 
 const CreatePost = (props) => {
-  return (
-    <div className='create-post'>
-      Post
+  const [open, setOpen] = useState(false);
+
+  // class if from PostOverlay component
+  const backdrop = (
+    <div
+      className='backdrop'
+      onClick={() => {
+        setOpen(false);
+      }}
+    >
     </div>
+  );
+
+  const postOverlay = <div className='create-overlay'>aa</div>;
+
+  return (
+    <>
+      <button
+        className='create-overlay__btn'
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        Post
+      </button>
+      {open && createPortal(backdrop, document.querySelector('#post-portal'))}
+      {open && createPortal(postOverlay, document.querySelector('#post-portal'))}
+    </>
   );
 };
 
