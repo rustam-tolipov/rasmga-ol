3c4f4368fb2518f674bd3c1407fe265eefda4c60
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-25 17:18:37 +0500

    Fix sign up form bug

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,26 +1,67 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useRef, useEffect } from 'react';
+
+import Axios from 'axios';
+import Cookies from 'js-cookie';
+
 import Button from '../UI/Button';
+
 import logo from '../../assets/logo.svg';
 import './Login.scss';
 
 const SignUp = () => {
-  const signUpHandler = () => {};
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    const isLogged = Cookies.get('jwt');
+    if (isLogged) navigate('/');
+  });
+
+  const signUpHandler = (e) => {
+    e.preventDefault();
+
+    Axios.post(
+      'https://rustam-social-media-rails-app.herokuapp.com/api/v1/auth/signup',
+      {
+        user: {
+          first_name: firstNameRef.current.value,
+          last_name: lastNameRef.current.value,
+          email: emailRef.current.value,
+          password: passwordRef.current.value,
+        },
+      }
+    )
+      .then((res) => {
+        Cookies.set('jwt', res.headers.authorization.slice(7));
+        localStorage.setItem('me', JSON.stringify(res.data));
+      })
+      .then(() => {
+        navigate('/');
+      })
+      .catch((err) => {
+        console.log('Could not sign up', err);
+      });
+  };
 
   return (
     <div className='login' onSubmit={signUpHandler}>
       <form className='login-form' action=''>
         <img className='login-img' src={logo} alt='Instagram logo' />
         <div>
-          <input type='text' placeholder='First Name' />
+          <input ref={firstNameRef} type='text' placeholder='First Name' />
         </div>
         <div>
-          <input type='text' placeholder='Last Name' />
+          <input ref={lastNameRef} type='text' placeholder='Last Name' />
         </div>
         <div>
-          <input type='email' placeholder='Email' />
+          <input ref={emailRef} type='email' placeholder='Email' />
         </div>
         <div>
-          <input type='password' placeholder='Password' />
+          <input ref={passwordRef} type='password' placeholder='Password' />
         </div>
 
         <Button>Sign Up</Button>
