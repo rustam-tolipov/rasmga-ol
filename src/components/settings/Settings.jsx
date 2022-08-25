79d2164f69cc5f8743448be128d3d639f5691837
Maqsud Tolipov <maqsudtolipov9@gmail.com>
2022-08-25 18:44:06 +0500

    Style settings page

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -1,5 +1,59 @@
+import { useState } from 'react';
+
+import avatar from '../../assets/avatar.jpg';
+import './Settings.scss';
+
 const Settings = () => {
-  return <div>a</div>;
+  const [data, setData] = useState(JSON.parse(localStorage.getItem('me')));
+
+  return (
+    <div className='settings'>
+      <form>
+        <div className='settings__img'>
+          <img
+            src={data.avatar.url === null ? avatar : data.avatar.url}
+            alt=''
+          />
+        </div>
+        <div className='settings__input-box'>
+          <input type='file' />
+        </div>
+
+        <p className='settings__name'>Name</p>
+        <div className='settings__input-box'>
+          <input type='text' />
+          <p>
+            Help people discover your account by using the name you're known by:
+            either your full name, nickname, or business name.
+          </p>
+        </div>
+
+        <p className='settings__name'>Username</p>
+        <div className='settings__input-box'>
+          <input type='text' />
+          <p>
+            In most cases, you'll be able to change your username back to
+            maqsudtolipov9 for another 14 days.
+          </p>
+        </div>
+
+        <p className='settings__name'>Bio</p>
+        <div className='settings__input-box'>
+          <textarea rows='4' cols='50' />
+          <p>
+            Prive your account by adding a short bio about yourself. This is a
+            good place to share a little bit about yourself and what you do.
+          </p>
+        </div>
+
+        <p className='settings__name'>Email</p>
+        <div className='settings__input-box'>
+          <input type='text' />
+          <p>This won't be displayed publicly.</p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Settings;
