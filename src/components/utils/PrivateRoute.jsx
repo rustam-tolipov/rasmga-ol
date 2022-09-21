a0b51edbe21e01b2ad47d20dd2f0d59f4fb05b60
Rustamjon Tolipov <rustamxontolipov788@gmail.com>
2022-09-21 21:33:12 +0500

    Update PrivateRoute.jsx

--- src/components/utils/PrivateRoute.jsx
+++ src/components/utils/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import { Outlet, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
 
 const PrivateRoute = (props) => {
-  const isLogged = Cookies.get('jwt');
+  const isLogged = Cookies.get('token');
 
   return isLogged ? <Outlet /> : <Navigate to='/login' />;
 };
