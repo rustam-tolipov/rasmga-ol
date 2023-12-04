68a343df84a1f00e3fe58ec488484c43f6f7a94f
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2023-12-04 20:18:27 +0500

    Add navigation bar

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,6 +1,17 @@
+import React from "react";
+import {
+  HiOutlineHome,
+  HiMagnifyingGlass,
+  HiOutlineFilm,
+  HiOutlineHeart,
+  HiOutlinePlusCircle,
+  HiBars3,
+  HiUserCircle,
+} from "react-icons/hi2";
 import { Outlet } from "react-router";
 
 import Sidebar from "./Sidebar";
+import { NavLink } from "react-router-dom";
 
 const AppLayout = () => {
   return (
@@ -10,8 +21,47 @@ const AppLayout = () => {
       <main className="overflow-scroll px-0 py-3 pt-6 sm:px-16 sm:pr-28">
         <Outlet />
       </main>
+
+      <Navigation />
     </div>
   );
 };
 
 export default AppLayout;
+
+const Navigation = () => {
+  return (
+    <div className="block sm:hidden">
+      <div className="fixed bottom-0 left-0 w-full  border-t border-slate-800 bg-[#121212] py-3">
+        <div className="flex h-full items-center justify-around">
+          <LinkItem icon={<HiOutlineHome className="text-3xl" />} to="/" />
+          <LinkItem
+            icon={<HiMagnifyingGlass className="text-3xl" />}
+            to="/search"
+          />
+          <LinkItem icon={<HiOutlineFilm className="text-3xl" />} to="/reels" />
+          <LinkItem
+            icon={<HiOutlineHeart className="text-3xl" />}
+            to="/notifications"
+          />
+          <LinkItem
+            icon={<HiUserCircle className="text-3xl" />}
+            to="/profile"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const LinkItem = ({ icon, to }) => {
+  return (
+    <NavLink
+      to={to}
+      className="flex items-center gap-4 text-xl"
+      activeClassName="text-white"
+    >
+      {icon}
+    </NavLink>
+  );
+};
