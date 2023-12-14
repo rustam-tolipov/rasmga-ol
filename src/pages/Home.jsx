5d317c5a3fde9ef0e83aa939db5cd7ff8a4ac180
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2023-12-14 13:56:33 +0500

    Prettier plugin for tailwind css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="px-3 py-3 pt-10">
+      <Highlights />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
+
+const Highlights = () => {
+  return (
+    <div className="border-1 h-12 w-full border-gray-50">
+      <h1>Highlight</h1>
+    </div>
+  );
+};
