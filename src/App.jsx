1fc2b8d3290fef7a70c14e2cd85f477b45cb6920
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2024-02-11 21:09:15 +0500

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
   return (
-    <div className="h-screen bg-[#121212] text-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl">
-        &ldquo;Rasmga-Ol&ldquo;{" "}
-        <span className="text-gray-400">is under construction</span>
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div className="h-screen bg-[#121212] text-gray-50 flex items-center justify-center">
+      Under Construction
+    </div>
+  );
+};
+
+export default PageNotFound;
