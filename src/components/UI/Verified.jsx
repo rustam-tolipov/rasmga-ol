52dd117f83498392386d8ca19738eda5ffe503cd
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2024-02-12 20:25:31 +0500

    Setup new project

--- src/components/UI/Verified.jsx
+++
@@ -1,9 +0,0 @@
-import { MdVerified } from 'react-icons/md';
-
-import './Verified.scss';
-
-const Verified = () => {
-  return <MdVerified color='#0095f6' />;
-};
-
-export default Verified;
--- src/redux/reduces/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import postsReducer from './postsReducer';
-
-export default combineReducers({
-  auth: authReducer,
-  posts: postsReducer,
-});
