1343777abc9e32b92d0452dd5ec2af44b580e4ba
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2024-02-12 20:44:42 +0500

    Add error message

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 function App() {
-  return <div className="text-xl">hi</div>;
+  return (
+    <div className="h-screen bg-[#121212] text-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl">
+        &ldquo;Rasmga-Ol&ldquo;{" "}
+        <span className="text-gray-400">is under construction</span>
+      </h1>
+    </div>
+  );
 }
 
 export default App;
