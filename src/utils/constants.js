b752764357abe0125634db4f49eb330e2eae5173
Rustam (Rostam) Tolipov <rustamxontolipov788@gmail.com>
2024-03-09 22:41:11 +0500

    Update isLocal variable to false

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,6 +2,6 @@ const BASE_LOCAL_URL = "http://localhost:3000/api/v1";
 const BASE_MAIN_URL = "https://rasmga-ol-api.onrender.com/api/v1";
 const BASE_URLL = "http://192.168.1.4:3000/api/v1";
 
-const isLocal = true;
+const isLocal = false;
 
 export const BASE_URL = isLocal ? BASE_LOCAL_URL : BASE_MAIN_URL;
